8aa07a6f3600ad432e95d11474c0464b29a55dec
arthurokuyat <arthurokuyat@gmail.com>
2025-04-25 20:35:51 +0300

    Delete rembg/main.py

--- rembg/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-# Create the FastAPI app instance
-app = FastAPI()
-
-# Define a simple route
-@app.get("/")
-def read_root():
-    return {"message": "Hello, World!"}
