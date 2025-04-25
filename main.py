a8c0c0f5ddb09ea1aec8bb5664d02cea475821d0
arthurokuyat <arthurokuyat@gmail.com>
2025-04-25 21:38:17 +0000

    Simplify main.py, add Gunicorn dependency, and update Render deployment instructions

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from fastapi.responses import StreamingResponse
 from rembg import remove
 from PIL import Image
 import io
-import uvicorn
 
 app = FastAPI()
 
@@ -21,9 +20,4 @@ async def remove_background(file: UploadFile = File(...)):
     buf = io.BytesIO()
     output_image.save(buf, format="PNG")
     buf.seek(0)
-
     return StreamingResponse(buf, media_type="image/png")
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))  # Use Render's PORT or default to 8000
-    uvicorn.run(app, host="0.0.0.0", port=port)
