598c9aaf09d3539bc1a97cf2381fa759d97ef0f2
arthurokuyat <arthurokuyat@gmail.com>
2025-04-26 00:50:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,6 @@
-import os
-from fastapi import FastAPI, File, UploadFile
-from fastapi.responses import StreamingResponse
-from rembg import remove
-from PIL import Image
-import io
-
+from fastapi import FastAPI
 app = FastAPI()
 
 @app.get("/")
 def read_root():
-    return {"message": "Send a POST request to /remove-bg with an image."}
-
-@app.post("/remove-bg")
-async def remove_background(file: UploadFile = File(...)):
-    contents = await file.read()
-    input_image = Image.open(io.BytesIO(contents)).convert("RGBA")
-    output_image = remove(input_image)
-
-    buf = io.BytesIO()
-    output_image.save(buf, format="PNG")
-    buf.seek(0)
-    return StreamingResponse(buf, media_type="image/png")
+    return {"Hello": "World"}
